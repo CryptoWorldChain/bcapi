22afb2c442dbeb10ba856d50e9d8446d47aa71ae
chijiema <chijiema@gmail.com>
2018-04-13 17:21:35 +0700

    del

--- src/main/java/org/fc/brewchain/bcapi/EncAPI.java
+++ src/main/java/org/fc/brewchain/bcapi/EncAPI.java
@@ -27,64 +27,12 @@ public interface EncAPI {
 
 	public byte[] hexDec(String data);
 	
-	
-	/**
-	 * 
-	 * @param pubKey
-	 * @param content
-	 * @return
-	 */
-	public String ecToAddress(String pubKey, String content);
-	/**
-	 * 
-	 * @param contentHash
-	 * @param r
-	 * @param s
-	 * @param v
-	 * @return
-	 */
 	public byte[] ecToAddress(byte[] contentHash, byte[] r,byte[] s,byte v);
-	/**
-	 * 
-	 * @param contentHash
-	 * @param signBase64
-	 * @return
-	 */
 	public byte[] ecToAddress(byte[] contentHash, String signBase64);
 	
-	/**
-	 * 
-	 * @param pubKey
-	 * @param content
-	 * @return
-	 */
-	public String ecToKeyBytes(String pubKey, String content);
-	/**
-	 * 
-	 * @param contentHash
-	 * @param r
-	 * @param s
-	 * @param v
-	 * @return
-	 */
 	public byte[] ecToKeyBytes(byte[] contentHash, byte[] r,byte[] s,byte v);
-	/**
-	 * 
-	 * @param contentHash
-	 * @param signBase64
-	 * @return
-	 */
 	public byte[] ecToKeyBytes(byte[] contentHash, String signBase64);
 	
-	/**
-	 * 
-	 * @param pubKey
-	 * @param content
-	 * @param r
-	 * @param s
-	 * @param v
-	 * @return
-	 */
 	public boolean ecVerify(String pubKey, String content, byte[] r,byte[] s,  byte v);
 
 	public byte[] sha3Encode(byte[] input);
